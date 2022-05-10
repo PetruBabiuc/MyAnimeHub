ba94a267552b5c2ee9c4fecba87802f37e6d061c
PetruBabiuc <petru.babiuc@student.tuiasi.ro>
2022-05-10 20:52:30 +0300

    Added test units

---
+++ Client/Tests/UnitTest.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Interfaces;
+using DBManagers;
+using DataClasses;
+using System.Text;
+
+namespace Tests
+{
+    [TestClass]
+    public class UnitTest
+    {
+        private static IMyAnimeHubDBManager _dbManager;
+        private static bool _loginResult;
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext context)
+        {
+            _dbManager = new DBManagerClientProxy();
+            _loginResult = _dbManager.Login("admin", "admin");
+        }
+
+        [TestMethod]
+        public void CorrectLogin()
+        {
+            Assert.AreEqual(true, _loginResult);
+        }
+
+        // [TestMethod]
+        // public void InvalidLogin()
+        // {
+        //     IMyAnimeHubDBManager _DBManagerProxy = new DBManagerClientProxy();
+        //     Assert.AreEqual(false, _DBManagerProxy.Login("QQQQQQQ", "QQQQQQQQ"));
+        // }
+
+        [TestMethod]
+        public void NewsPage()
+        {
+            Assert.IsTrue(_dbManager.GetNews(0).Count>0);
+        }
+
+        [TestMethod]
+        public void TooBigNewsPageIndex()
+        {
+            Assert.IsTrue(_dbManager.GetNews(5000).Count == 0);
+        }
+
+        [TestMethod]
+        public void AnimePage()
+        {
+            Assert.IsTrue(_dbManager.GetAnimeList("", 0).Count > 0);
+        }
+
+        [TestMethod]
+        public void TooBigAnimePageIndex()
+        {
+            Assert.IsTrue(_dbManager.GetAnimeList("", 5000).Count == 0);
+        }
+
+        [TestMethod]
+        public void ReviewsPage()
+        {
+            Assert.IsTrue(_dbManager.GetReviews(1, 0).Count > 0);
+        }
+
+        [TestMethod]
+        public void TooBigReviewsPageIndex()
+        {
+            Assert.IsTrue(_dbManager.GetReviews(1, 5000).Count == 0);
+        }
+
+        [TestMethod]
+        public void SpecificAnime()
+        {
+            Anime anime = _dbManager.GetSpecificAnime(1);
+            Assert.AreEqual(anime.EpisodesNumber, 500);
+            Assert.AreEqual(anime.ID, 1);
+            Assert.AreEqual(anime.Author, "Masashi Kishimoto");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void InvalidNameOfUserAccount()
+        {
+            UserAccount account = new UserAccount();
+            account.Name = "12";
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void InvalidUserName()
+        {
+            UserAccount account = new UserAccount();
+            account.UserName = "12";
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void InvalidBirthDate()
+        {
+            UserAccount account = new UserAccount();
+            account.BirthDate = "1/13/2000";
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void InvalidMail()
+        {
+            UserAccount account = new UserAccount();
+            account.BirthDate = "florinel@abcd.kr";
+        }
+
+        [TestMethod]
+        public void ValidUserAccount()
+        {
+            UserAccount account = new UserAccount();
+            account.Name = "Ronaldinho99";
+            account.Mail = "andreiflorinel@yahoo.ro";
+            account.BirthDate = "5/6/1999";
+            account.UserName = "admin68";
+            account.Password = "admin68";
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void EmptyReviewTitle()
+        {
+            Review review = new Review();
+            review.Title = "";
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ReviewTitleTooLong()
+        {
+            Review review = new Review();
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i = 0; i < 50; ++i)
+                stringBuilder.Append("A");
+            review.Title = stringBuilder.ToString();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void EmptyReviewContent()
+        {
+            Review review = new Review();
+            review.Content = "";
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ReviewContentTooLong()
+        {
+            Review review = new Review();
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i = 0; i < 1000; ++i)
+                stringBuilder.Append("0123456789");
+            stringBuilder.Append("X");
+            review.Content = stringBuilder.ToString();
+        }
+
+        [TestMethod]
+        public void ValidReview()
+        {
+            Review review = new Review();
+            review.Anime = "Bleach";
+            review.Author = "Florinel";
+            review.Content = "adfsdfs";
+            review.Title = "OK!";
+        }
+    }
+}
---
+++ Server/Tests/UnitTest.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CommonHelpers;
+using DataClasses;
+using System.Collections.Generic;
+using Interfaces;
+using DBManagers;
+using InitialClientActions;
+
+namespace Tests
+{
+    [TestClass]
+    public class UnitTest
+    {
+        private static IMyAnimeHubDBManagerAdapter _dbManagerAdapter = new MyAnimeHubDBManagerJsonAdapter();
+        private static string _resultString;
+        private static InitialAction _loginResult;
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext context)
+        {
+            UserAccount account = new UserAccount();
+            account.UserName = "admin";
+            account.Password = "admin";
+            var dict = new Dictionary<string, string>
+            {
+                {"account", JsonUtilities.ToJson(account) },
+                {"requestFunction", "Login" }
+            };
+            _loginResult = _dbManagerAdapter.LoginOrRegister(JsonUtilities.ToJson(dict), out _resultString);
+        }
+
+        [TestMethod]
+        public void EncryptDecrypt()
+        {
+            string pass = "pass";
+            string rawText = "text";
+            string encryptedText = Cryptography.Encrypt(rawText, pass);
+            Assert.AreNotEqual(rawText, encryptedText);
+            string decryptedText = Cryptography.Decrypt(encryptedText, pass);
+            Assert.AreEqual(rawText, decryptedText);
+        }
+
+        [TestMethod]
+        public void SerializeDeserialize()
+        {
+            UserAccount account = new UserAccount();
+            account.BirthDate = "5/6/1999";
+            account.Mail = "andreiflorinel@yahoo.ro";
+            account.Name = "Georgel";
+            account.UserName = "admin";
+            account.Password = "admin";
+
+            string json = JsonUtilities.ToJson(account);
+
+            var dict = JsonUtilities.FromJson<Dictionary<string, string>>(json);
+            Assert.AreEqual(dict["Name"], account.Name);
+            Assert.AreEqual(dict["BirthDate"], account.BirthDate);
+            Assert.AreEqual(dict["Mail"], account.Mail);
+            Assert.AreEqual(dict["UserName"], account.UserName);
+            Assert.AreEqual(dict["Password"], account.Password);
+
+            UserAccount deserializedAccount = JsonUtilities.FromJson<UserAccount>(json);
+            Assert.AreEqual(deserializedAccount.Name, account.Name);
+            Assert.AreEqual(deserializedAccount.BirthDate, account.BirthDate);
+            Assert.AreEqual(deserializedAccount.Mail, account.Mail);
+            Assert.AreEqual(deserializedAccount.UserName, account.UserName);
+            Assert.AreEqual(deserializedAccount.Password, account.Password);
+        }
+
+        [TestMethod]
+        public void SuccessfulLogin()
+        {
+            Assert.AreEqual(InitialAction.LoginSuccessful, _loginResult);
+            Assert.AreEqual(_resultString, "{\"result\":\"Succes!\"}");
+        }
+
+        [TestMethod]
+        public void FailedLogin()
+        {
+            UserAccount account = new UserAccount();
+            account.UserName = "adminnn";
+            account.Password = "admin";
+            var dict = new Dictionary<string, string>
+            {
+                {"account", JsonUtilities.ToJson(account) },
+                {"requestFunction", "Login" }
+            };
+            IMyAnimeHubDBManagerAdapter dbManager = new MyAnimeHubDBManagerJsonAdapter();
+            string resultString;
+            InitialAction result = dbManager.LoginOrRegister(JsonUtilities.ToJson(dict), out resultString);
+            Assert.AreEqual(InitialAction.LoginFailed, result);
+            Assert.AreEqual(resultString, "{\"result\":\"Combinatia dintre nume si parola este invalida!\"}");
+        }
+
+        [TestMethod]
+        public void GetUserAccount()
+        {
+            string data, result;
+            var dictionary = new Dictionary<string, string>();
+            dictionary["requestFunction"] = "GetUserAccount";
+            dictionary["user"] = "admin";
+            dictionary["pass"] = "admin";
+            data = JsonUtilities.ToJson(dictionary);
+            _dbManagerAdapter.ProcessRequest(data, out result);
+            UserAccount resultAccount = JsonUtilities.FromJson<UserAccount>(result);
+            Assert.AreEqual(resultAccount.Mail, null);
+            Assert.AreEqual(resultAccount.Name, "Petru");
+            Assert.AreEqual(resultAccount.UserName, "admin");
+            Assert.AreEqual(resultAccount.Password, "admin");
+            Assert.AreEqual(resultAccount.BirthDate, "6/7/1999");
+        }
+    }
+}
